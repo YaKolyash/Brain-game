5d3222a9b91860af9b6b8244bde2cff2699c9a39
Nikolay <nefigdelat1>
2023-12-16 14:48:42 +0300

    eslintrc fix

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,1 @@
-const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-export default random;
+export const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
