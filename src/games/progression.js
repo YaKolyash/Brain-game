951b06a95d082a9173fadfbf192abd2a846e30ce
Nikolay <nefigdelat1>
2023-12-24 18:48:21 +0300

    added the step constant

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,11 +3,11 @@ import random from '../utils.js';
 
 const description = 'What number is missing in the progression?';
 
-const getArithmeticProgression = (min, randomIndex, progressionLength) => {
+const getArithmeticProgression = (min, step, progressionLength) => {
   const progression = [min];
 
   for (let i = 0; i < progressionLength; i += 1) {
-    progression.push(progression[i] + randomIndex);
+    progression.push(progression[i] + step);
   }
 
   return progression;
@@ -16,8 +16,9 @@ const getArithmeticProgression = (min, randomIndex, progressionLength) => {
 const getQuestionAndAnswer = () => {
   const progressionLength = 10;
   const numberProgression = random(0, 50);
+  const step = random(0, 10);
   const randomIndex = random(0, progressionLength - 1);
-  const progression = getArithmeticProgression(numberProgression, randomIndex, progressionLength);
+  const progression = getArithmeticProgression(numberProgression, step, progressionLength);
 
   const correctAnswer = String(progression[randomIndex]);
   progression[randomIndex] = '..';
